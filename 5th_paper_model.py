6f6ff6dc53af01d45881020d01909f0adfd0b8f4
YYu HHwan KKim <31001511+Kimyuhwanpeter@users.noreply.github.com>
2022-03-15 16:46:41 +0900

    Update 5th_paper_model.py

--- 5th_paper_model.py
+++ 5th_paper_model.py
@@ -17,80 +17,80 @@ def residual_block(input):
     
     return h + input
 
-def Depth_wise_Unet(input_shape=(512, 512, 3)):
+def Depth_wise_Unet(input_shape=(512, 512, 3), pretrained=True):
 
     h = inputs = tf.keras.Input(input_shape)
 
     h = tf.pad(h, [[0,0],[1,1],[1,1],[0,0]], "REFLECT")
-    h = tf.keras.layers.Conv2D(filters=64, kernel_size=3, strides=1, use_bias=False, name="conv1")(h)
+    h = tf.keras.layers.Conv2D(filters=64, kernel_size=3, strides=1, use_bias=True, name="conv1")(h)
     h = tf.keras.layers.BatchNormalization()(h)
     h = tf.keras.layers.ReLU(max_value=6.)(h)
 
     h = tf.pad(h, [[0,0],[1,1],[1,1],[0,0]], "REFLECT")
-    conv2 = tf.keras.layers.Conv2D(filters=64, kernel_size=3, strides=1, use_bias=False, name="conv2")(h)
+    conv2 = tf.keras.layers.Conv2D(filters=64, kernel_size=3, strides=1, use_bias=True, name="conv2")(h)
     conv2 = tf.keras.layers.BatchNormalization()(conv2)
     conv2 = tf.keras.layers.ReLU(max_value=6.)(conv2)
     
     h = tf.keras.layers.MaxPool2D((2,2), strides=2, name="maxpool1")(conv2)
 
     h = tf.pad(h, [[0,0],[1,1],[1,1],[0,0]], "REFLECT")
-    h = tf.keras.layers.Conv2D(filters=128, kernel_size=3, strides=1, use_bias=False, name="conv3")(h)
+    h = tf.keras.layers.Conv2D(filters=128, kernel_size=3, strides=1, use_bias=True, name="conv3")(h)
     h = tf.keras.layers.BatchNormalization()(h)
     h = tf.keras.layers.ReLU(max_value=6.)(h)
 
     h = tf.pad(h, [[0,0],[1,1],[1,1],[0,0]], "REFLECT")
-    conv4 = tf.keras.layers.Conv2D(filters=128, kernel_size=3, strides=1, use_bias=False, name="conv4")(h)
+    conv4 = tf.keras.layers.Conv2D(filters=128, kernel_size=3, strides=1, use_bias=True, name="conv4")(h)
     conv4 = tf.keras.layers.BatchNormalization()(conv4)
     conv4 = tf.keras.layers.ReLU(max_value=6.)(conv4)
 
     h = tf.keras.layers.MaxPool2D((2,2), strides=2, name="maxpool2")(conv4)
 
     h = tf.pad(h, [[0,0],[1,1],[1,1],[0,0]], "REFLECT")
-    h = tf.keras.layers.Conv2D(filters=256, kernel_size=3, strides=1, use_bias=False, name="conv5")(h)
+    h = tf.keras.layers.Conv2D(filters=256, kernel_size=3, strides=1, use_bias=True, name="conv5")(h)
     h = tf.keras.layers.BatchNormalization()(h)
     h = tf.keras.layers.ReLU(max_value=6.)(h)
 
     h = tf.pad(h, [[0,0],[1,1],[1,1],[0,0]], "REFLECT")
-    h = tf.keras.layers.Conv2D(filters=256, kernel_size=3, strides=1, use_bias=False, name="conv6")(h)
+    h = tf.keras.layers.Conv2D(filters=256, kernel_size=3, strides=1, use_bias=True, name="conv6")(h)
     h = tf.keras.layers.BatchNormalization()(h)
     h = tf.keras.layers.ReLU(max_value=6.)(h)
 
     h = tf.pad(h, [[0,0],[1,1],[1,1],[0,0]], "REFLECT")
-    conv7 = tf.keras.layers.Conv2D(filters=256, kernel_size=3, strides=1, use_bias=False, name="conv7")(h)
+    conv7 = tf.keras.layers.Conv2D(filters=256, kernel_size=3, strides=1, use_bias=True, name="conv7")(h)
     conv7 = tf.keras.layers.BatchNormalization()(conv7)
     conv7 = tf.keras.layers.ReLU(max_value=6.)(conv7)
 
     h = tf.keras.layers.MaxPool2D((2,2), strides=2, name="maxpool3")(conv7)
 
     h = tf.pad(h, [[0,0],[1,1],[1,1],[0,0]], "REFLECT")
-    h = tf.keras.layers.Conv2D(filters=512, kernel_size=3, strides=1, use_bias=False, name="conv8")(h)
+    h = tf.keras.layers.Conv2D(filters=512, kernel_size=3, strides=1, use_bias=True, name="conv8")(h)
     h = tf.keras.layers.BatchNormalization()(h)
     h = tf.keras.layers.ReLU(max_value=6.)(h)
 
     h = tf.pad(h, [[0,0],[1,1],[1,1],[0,0]], "REFLECT")
-    h = tf.keras.layers.Conv2D(filters=512, kernel_size=3, strides=1, use_bias=False, name="conv9")(h)
+    h = tf.keras.layers.Conv2D(filters=512, kernel_size=3, strides=1, use_bias=True, name="conv9")(h)
     h = tf.keras.layers.BatchNormalization()(h)
     h = tf.keras.layers.ReLU(max_value=6.)(h)
 
     h = tf.pad(h, [[0,0],[1,1],[1,1],[0,0]], "REFLECT")
-    conv10 = tf.keras.layers.Conv2D(filters=512, kernel_size=3, strides=1, use_bias=False, name="conv10")(h)
+    conv10 = tf.keras.layers.Conv2D(filters=512, kernel_size=3, strides=1, use_bias=True, name="conv10")(h)
     conv10 = tf.keras.layers.BatchNormalization()(conv10)
     conv10 = tf.keras.layers.ReLU(max_value=6.)(conv10)
 
     h = tf.keras.layers.MaxPool2D((2,2), strides=2, name="maxpool4")(conv10)
 
     h = tf.pad(h, [[0,0],[1,1],[1,1],[0,0]], "REFLECT")
-    h = tf.keras.layers.Conv2D(filters=512, kernel_size=3, strides=1, use_bias=False, name="conv11")(h)
+    h = tf.keras.layers.Conv2D(filters=512, kernel_size=3, strides=1, use_bias=True, name="conv11")(h)
     h = tf.keras.layers.BatchNormalization()(h)
     h = tf.keras.layers.ReLU(max_value=6.)(h)
 
     h = tf.pad(h, [[0,0],[1,1],[1,1],[0,0]], "REFLECT")
-    h = tf.keras.layers.Conv2D(filters=512, kernel_size=3, strides=1, use_bias=False, name="conv12")(h)
+    h = tf.keras.layers.Conv2D(filters=512, kernel_size=3, strides=1, use_bias=True, name="conv12")(h)
     h = tf.keras.layers.BatchNormalization()(h)
     h = tf.keras.layers.ReLU(max_value=6.)(h)
 
     h = tf.pad(h, [[0,0],[1,1],[1,1],[0,0]], "REFLECT")
-    conv13 = tf.keras.layers.Conv2D(filters=512, kernel_size=3, strides=1, use_bias=False, name="conv13")(h)
+    conv13 = tf.keras.layers.Conv2D(filters=512, kernel_size=3, strides=1, use_bias=True, name="conv13")(h)
     conv13 = tf.keras.layers.BatchNormalization()(conv13)
     conv13 = tf.keras.layers.ReLU(max_value=6.)(conv13)
 
@@ -179,8 +179,33 @@ def Depth_wise_Unet(input_shape=(512, 512, 3)):
 
     final_output = tf.concat([crop_h, weed_h, object_h], -1)
 
+    model = tf.keras.Model(inputs=inputs, outputs=final_output)
+
+    backbone = tf.keras.applications.VGG16(input_shape=(224, 224, 3))
+
+    if pretrained:
+        model.get_layer("conv1").set_weights(backbone.get_layer("block1_conv1").get_weights())
+        model.get_layer("conv2").set_weights(backbone.get_layer("block1_conv2").get_weights())
+        model.get_layer("conv3").set_weights(backbone.get_layer("block2_conv1").get_weights())
+        model.get_layer("conv4").set_weights(backbone.get_layer("block2_conv2").get_weights())
+        model.get_layer("conv5").set_weights(backbone.get_layer("block3_conv1").get_weights())
+        model.get_layer("conv6").set_weights(backbone.get_layer("block3_conv2").get_weights())
+        model.get_layer("conv7").set_weights(backbone.get_layer("block3_conv3").get_weights())
+        model.get_layer("conv8").set_weights(backbone.get_layer("block4_conv1").get_weights())
+        model.get_layer("conv9").set_weights(backbone.get_layer("block4_conv2").get_weights())
+        model.get_layer("conv10").set_weights(backbone.get_layer("block4_conv3").get_weights())
+        model.get_layer("conv11").set_weights(backbone.get_layer("block5_conv1").get_weights())
+        model.get_layer("conv12").set_weights(backbone.get_layer("block5_conv2").get_weights())
+        model.get_layer("conv13").set_weights(backbone.get_layer("block5_conv3").get_weights())
+        print("================================================================================")
+        print("** The encoder part is used based on the pretrained VGG-16 (imagenet dataset).**")
+        print("================================================================================")
+    else:
+        print("================================================================================")
+        print("** The encoder part is used based on the VGG-16 (Train from scretch).**")
+        print("================================================================================")
+
     return tf.keras.Model(inputs=inputs, outputs=final_output)
 
 model = Depth_wise_Unet()
 model.summary()
-m = tf.keras.applications.MobileNetV2
